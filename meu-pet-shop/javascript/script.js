58c94483dc8101fa701cd1e2412fd3ad688e249f
AnaRamos2110 <anabeatriz055@hotmail.com>
2025-12-16 22:29:54 -0300

    Adicionando carrossel e menu hamburger com javascript

---
+++ meu-pet-shop/javascript/script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll(".banner");
+let slideIndex = 0;
+
+function showSlides(){
+    for (let i = 0; i< slides.length; i++) {
+        slides[i].classList.remove("active");
+        }
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1
+
+    slides[slideIndex - 1].classList.add("active");
+    
+    setTimeout(showSlides, 3500);
+
+}
+
+showSlides()
